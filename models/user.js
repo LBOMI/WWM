01cee7375649339b02f718a37a638d2b84c63c1b
LBOMI <125543534+LBOMI@users.noreply.github.com>
2024-12-30 17:03:11 +0900

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,12 +1,12 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
+
   var user = sequelize.define('user', {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
-      // primaryKey: true
     },
-    email: { //이메일 컬럼을 고유키로 두고, email 양식이 맞는지 확인하는 validate 추가
+    email: { 
       type: DataTypes.STRING,
       allowNull: false
     },
@@ -19,7 +19,10 @@ module.exports = (sequelize, DataTypes) => {
     }
   });
   user.removeAttribute('id');
+
+
   return user;
+
 };
 
 
